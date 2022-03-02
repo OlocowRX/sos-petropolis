0fe04d61c8350eee5ef6a79822f9b117056a22a1
gabriel tavares <gabriel29t@gmail.com>
2022-03-02 17:48:47 -0300

    Add index show new e create for CampaignsController. Also added their current views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :campaigns
+  has_many :campaigns, dependent: :destroy
   has_many :donations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :campaigns, only: %i[new create]
+  resources :campaigns, only: %i[index show new create]
 end
