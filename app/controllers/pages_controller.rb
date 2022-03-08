7d8ae619a603d61d3a86f103a68bc01ea5faa752
gabriel tavares <gabriel29t@gmail.com>
2022-03-08 13:19:51 -0300

    almost done with carousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @campaigns = Campaign.last(4)
+    @campaigns = Campaign.last(5)
     @donations_cash = Donation.where(donation_type: "dinheiro").sum(:quantity) / 1000
     @donations_food = Donation.where(donation_type: "comida").sum(:quantity)
     @donations_cloth = Donation.where(donation_type: "roupas").sum(:quantity)
